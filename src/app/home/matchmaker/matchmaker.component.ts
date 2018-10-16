de5e21add218809b65e995c8fb68225aae7589f0
Ørjan Ertkjern <orjanert@gmail.com>
2018-10-16 09:00:34 +0200

    matchmaker styling

---
+++ src/app/home/matchmaker/matchmaker.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-matchmaker',
+  styleUrls: ['matchmaker.component.scss'],
+  templateUrl: 'matchmaker.component.html'
+})
+export class MatchmakerComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+}

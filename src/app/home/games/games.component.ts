af1d9b02bec76bb91b5ec68438306ecf73a38fd2
Ørjan Ertkjern <orjanert@gmail.com>
2018-10-18 09:41:55 +0200

    Finished register match and ELO rating

--- src/app/home/games/games.component.ts
+++ src/app/home/games/games.component.ts
@@ -17,7 +17,29 @@ export class GamesComponent implements OnInit {
     this.loadMatches();
   }
 
-  private loadMatches(){
+  registerWinner(player1Won: boolean, match: MatchModel) {
+    this.matchmaker.registerWinner(player1Won, match).then(result => {
+      console.log('match complete');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  deleteMatch(match: MatchModel) {
+    if (match.id) {
+      if (confirm('Are you sure?')) {
+        this.matchmaker.deleteMatch(match.id).then(result => {
+          console.log('delted');
+        }, err => {
+          console.log(err);
+        });
+      } else {
+        console.log('abort');
+      }
+    }
+  }
+
+  private loadMatches() {
     this.matchmaker.getMatchesByDate().subscribe(result => {
       this.matches = result;
     }, error => {
--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,5 +1,6 @@
 export class UserModel {
   uid?: string;
+  isPlaying: boolean;
   email: string;
   username: string;
   name: string;

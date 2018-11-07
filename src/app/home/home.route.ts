455e141bf0b3a6eae966092317e5b74f63fa01ec
Ørjan Ertkjern <orjanert@gmail.com>
2018-11-07 15:00:18 +0100

    more great design

--- src/app/home/home.route.ts
+++ src/app/home/home.route.ts
@@ -2,12 +2,29 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {HomeComponent} from './home.component';
 import {AuthGuard} from '../shared/services/authGuard';
+import {MatchmakerComponent} from './matchmaker/matchmaker.component';
+import {ScoreComponent} from './score/score.component';
+import {HistoryComponent} from './history/history.component';
 
 const HOME: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: MatchmakerComponent,
+      },
+      {
+        path: 'leader-board',
+        component: ScoreComponent,
+      },
+      {
+        path: 'history',
+        component: HistoryComponent
+      }
+    ]
   }
 ];
 

e3032f957e85aa6d641b1fe3db647a0b47f28c1c
Ørjan Ertkjern <orjanert@gmail.com>
2018-11-26 21:44:23 +0100

    Added release note

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
+  version: '1.1.0b',
   firebase: {
-    apiKey: 'AIzaSyD1L1D_iM8n46kapVqA4yY5XPhSXFS4XBE',
-    authDomain: 'evry-fuss-test.firebaseapp.com',
-    databaseURL: 'https://evry-fuss-test.firebaseio.com',
-    projectId: 'evry-fuss-test',
-    storageBucket: 'evry-fuss-test.appspot.com',
-    messagingSenderId: '429338436802'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };

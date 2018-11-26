1be370206f7a7ac76ac5efcf54b52ec7b9e2605f
Ørjan Ertkjern <orjanert@gmail.com>
2018-11-26 21:57:46 +0100

    Environment setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  version: '1.1.0b',
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
